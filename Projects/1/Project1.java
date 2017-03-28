852ade61351cfd2963e67707bfafa8768d9146a8
katedominguez <kdoming3@gmail.com>
2017-03-28 17:12:18 -0400

    Update Project1.java

--- Projects/1/Project1.java
+++ Projects/1/Project1.java
@@ -10,47 +10,45 @@ import java.util.Scanner;
 public class Project1{ 
   public static void main(String[] args){
 	  
-	//Create Scanner
+    //Create Scanner
     Scanner input= new Scanner(System.in);
 	
-	//Get the size of both arrays
-	int sizeFirst= input.nextInt();
-	int sizeSecond= input.nextInt();
+    //Get the size of both arrays
+    int sizeFirst= input.nextInt();
+    int sizeSecond= input.nextInt();
 	 
-	//Get the elements of both arrays
-	double[] vFirst= new double[sizeFirst];
-	double[] vSecond= new double[sizeSecond];
+    //Get the elements of both arrays
+    double[] vFirst= new double[sizeFirst];
+    double[] vSecond= new double[sizeSecond];
 	
-	//Put elements into the arrays
-	for(int x=0; x< sizeFirst; x++)
-	{
-	  vFirst[x]= input.nextDouble();
-	}
-	for(int y=0; y< sizeSecond; y++)
-	{
-	  vSecond[y]= input.nextDouble();
-	}
+    //Put elements into the arrays
+    for(int x=0; x< sizeFirst; x++)
+      vFirst[x]= input.nextDouble();
+    for(int y=0; y< sizeSecond; y++)
+      vSecond[y]= input.nextDouble();
 	
-	//Call method convolveVectors
-	convolveVectors(vFirst, vSecond);
-
+     //Call method convolveVectors
+     convolveVectors(vFirst, vSecond);
   }
   public static double[] convolveVectors(double[] vFirst, double[] vSecond) {
     double[] vResult = new double[(vFirst.length + vSecond.length) - 1];
+    // create for loop to go through the index
     for(int i=0; i< vResult.length; i++)
-	{
-	  for(int shift=0; shift< vResult.length; shift++)
-	    {
-		  try{
-		    vResult[i]+= vFirst[i-shift] * vSecond[shift];
-		  } catch(IndexOutOfBoundsException e){
-		  }
-	    }
+    {
+      //Create for loop to go through shifts
+      for(int shift=0; shift< vResult.length; shift++)
+      {
+	//Implement try catch to prevent going out of bounds
+        try{
+	  vResult[i]+= vFirst[i-shift] * vSecond[shift];
+	} catch(IndexOutOfBoundsException e){
 	}
-	for(int p=0; p< vResult.length; p++)
-	  System.out.print(vResult[p] + " ");
+      }
+    }
+    //Print out the elements of the result array
+    for(int p=0; p< vResult.length; p++)
+      System.out.print(vResult[p] + " ");
     return vResult;  
   }
-
 }
 	
