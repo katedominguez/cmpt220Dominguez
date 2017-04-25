b5ca72c3ed70627233b884ef4bf1bb07c473fe27
katedominguez <kdoming3@gmail.com>
2017-04-24 23:23:54 -0400

    Update Lab4Problem1.java

--- Labs/Lab 4/Lab4Problem1.java
+++ Labs/Lab 4/Lab4Problem1.java
@@ -8,13 +8,17 @@ March 24, 2017
 import java.util.Scanner;
 public class Lab4Problem1{ 
   public static void main(String[] args){
+    //Create scanner
     Scanner input= new Scanner(System.in);
+    //Print heading and get temperatures from the user
     System.out.println("Celsius          Fahrenheit          |          Fahrenheit          Celsius");
     System.out.println("___________________________________________________________________________");
     double celsius= input.nextDouble();
     double fahrenheit= input.nextDouble();
-	double ctof= celsiusToFahrenheit(celsius);
-	double ftoc= fahrenheitToCelsius(fahrenheit);
+    //Call the methods
+    double ctof= celsiusToFahrenheit(celsius);
+    double ftoc= fahrenheitToCelsius(fahrenheit);
+    //Print out the answers
     System.out.print("                                       " + ctof + "                          " + ftoc);
   }
   public static double celsiusToFahrenheit(double celsius){
