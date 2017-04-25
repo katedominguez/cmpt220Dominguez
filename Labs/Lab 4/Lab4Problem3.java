3e37f818fbb22cbe0b5b10e6b0750ec7e008389e
katedominguez <kdoming3@gmail.com>
2017-04-24 23:27:48 -0400

    Update Lab4Problem3.java

--- Labs/Lab 4/Lab4Problem3.java
+++ Labs/Lab 4/Lab4Problem3.java
@@ -8,17 +8,23 @@ March 24, 2017
 import java.util.Scanner;
 public class Lab4Problem3{ 
   public static void main(String[] args){
+    //Create a scanner
     Scanner input= new Scanner(System.in);
+    //Create the array with a size of 10
     double[] numbers= new double[10];
+    //Ask the user for the 10 numbers
     System.out.println("Please enter 10 numbers: ");
+    //Add the 10 numbers to the array
     for(int i=0; i< numbers.length; i++)
     {
       numbers[i]= input.nextDouble();
     }
+    //Call and print the min method
     System.out.println(min(numbers));
   }
   public static double min(double[] numbers){
     double smallest= numbers[0];
+    //Find the smallest number in the array
     for(int x=0; x<= numbers.length-1; x++)
     {
       if(numbers[x] < smallest)
