8c38101517bc53d0f62431545196642de85269b5
katedominguez <kdoming3@gmail.com>
2017-04-24 23:25:26 -0400

    Update Lab4Problem2.java

--- Labs/Lab 4/Lab4Problem2.java
+++ Labs/Lab 4/Lab4Problem2.java
@@ -8,15 +8,18 @@ March 24, 2017
 import java.util.Scanner;
 public class Lab4Problem2{ 
   public static void main(String[] args){
+    //Create a Scanner
     Scanner input= new Scanner(System.in);
     System.out.println("Please enter a string: ");
     String s= input.nextLine();
+    //Call method countLetters
     System.out.println(countLetters(s));
   }
   public static int countLetters(String s){
+    //Create a variable to track the number of letters in the string
     int num=0;
+    //Count the number of letters in the string
     for(int i=0; i<= s.length(); i++){
-      //if(s.charAt(i)!= 0 && s.charAt(i)!= 1 && s.charAt(i)!= 2 && s.charAt(i)!= 3 && s.charAt(i)!= 4 && s.charAt(i)!= 5 && s.charAt(i)!= 6 && s.charAt(i)!= 7 && s.charAt(i)!= 8 && s.charAt(i)!= 9)
         num++;
     }
     return (num-1);
