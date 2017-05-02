951ab2acb46fd5761f2074f140ef1476f0dd6ce2
katedominguez <kdoming3@gmail.com>
2017-05-02 10:53:20 -0400

    Update Lab5Problem3.java

--- Labs/Lab 5/Lab5Problem3.java
+++ Labs/Lab 5/Lab5Problem3.java
@@ -10,17 +10,22 @@ public class Lab5Problem3{
   public static void main(String[] args){
     //Create scanner
     Scanner input= new Scanner(System.in);
+    //Create ArrayList
     List<int> list = new ArrayList<int>();
+    //Get elements of the ArrayList
     System.out.println("Enter list: ");
     list.length= input.nextInt();
     while (input.hasNextInt()) {
       list.add(input.nextInt());
     }
+    //Call partition method and print out result
     System.out.println(partition(list));
   }
   public static int partition(int[] list){
     int p= (list.length)/2;
+    //Create result arraylist
     List<int> result= new ArrayList<int>();
+    //Get result
     for(int x=0; x<=list.length; x++){
       for(int y=0; y<=list.length; y++){
         if(list[x] < list[y])
