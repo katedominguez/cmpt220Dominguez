375ad7f9b440387e66d1ac212f5be2137677e6ef
katedominguez <kdoming3@gmail.com>
2017-05-02 10:55:19 -0400

    Update Lab5Problem4.java

--- Labs/Lab 5/Lab5Problem4.java
+++ Labs/Lab 5/Lab5Problem4.java
@@ -10,19 +10,24 @@ public class Lab5Problem4{
   public static void main(String[] args){
     //Create scanner
     Scanner input= new Scanner(System.in);
+    //Get the dimensions of the array
     System.out.println("Enter the number of rows and columns of the array: ");
     int r= input.nextInt();
     int c = input.nextInt();
     double[][] a = new double[r][c];
+    //Get the elements of the array
     System.out.println("Enter the array: ");
     for (int x = 0; x < r; x++)
       for (int y = 0; y < c; y++)
          a[x][y] = input.nextDouble();
+    //Call locateLargest method and print out the result
     System.out.println("The location of the largest element is at " + java.util.Arrays.toString(locateLargest(a)));
   }
   public static int[] locateLargest(double[][] a){
+    //Create location and largest
     int[] location = new int[] { 0, 0 };
     double largest = a[0][0];
+    //Find the largest and the location
     for (int b = 0; b < a.length; b++) {
       for (int c = 0; c < a[b].length; c++) {
         if (a[b][c] > largest) {
